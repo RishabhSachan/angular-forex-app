d62c15c494a2b8499f481c6716fcae0eff352e31
RishabhSachan <rishabh_sachan@yahoo.co.in>
2022-03-15 23:18:01 +0530

    4 files modified, 1 added

--- src/app/forex/forex.component.ts
+++ src/app/forex/forex.component.ts
@@ -9,14 +9,30 @@ import { ForexService } from './forex.service';
 export class ForexComponent implements OnInit {
   val: any;
   rates: any;
-  src='INR';
+  src = 'INR';
+  keys: any;
+  map = {};
   constructor(private frxServ: ForexService) {}
 
   ngOnInit() {
     this.val = this.frxServ.demo;
-    this.frxServ.getRates(this.src).subscribe(val=> {
+  }
+  public getRates() {
+    this.map = {};
+    this.frxServ.getRates(this.src).subscribe((val) => {
       console.log(val.rates);
       this.rates = val.rates;
+      this.keys = Object.keys(val.rates);
     });
+  }
+  public getConvert(e,curr) {
+    const amount = e.target.value;
+    this.map[curr] = {src: Number(amount), target: Number(amount)*this.rates[curr]};
+    console.log(this.map)
+  }
+  public getConvertFromTarget(e,curr) {
+    const amount = e.target.value;
+    this.map[curr] = {src: Number(amount)/this.rates[curr], target: Number(amount)};
+    console.log(this.map)
   }
 }
--- src/app/forex/forex.service.ts
+++ src/app/forex/forex.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ForexService {
   public demo = 5;
   constructor(private http: HttpClient) {}
 
   getRates(src: string): Observable<any> {
-    return this.http.get('https://api.exchangerate-api.com/v4/latest/'+src);
+    console.log('fetched' + src);
+    return this.http.get('https://api.exchangerate-api.com/v4/latest/' + src);
   }
-
 }
